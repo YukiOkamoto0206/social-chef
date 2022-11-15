94135f08a8a6331781bcf628e92c084a7ad6ef23
Miguel A. Francisco <mfrancisco@csumb.edu>
2022-11-15 13:39:39 -0800

    Updated the name of files to improve readability

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('login');
 });
 
 app.get('/create', (req, res) => {
