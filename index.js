cbff88c41b8fca8fb802e0fddc9d5ace051f84c2
Aaron Johnson <you@example.com>
2022-11-17 12:27:13 -0800

    feat: fetched the recipes api and test output on ejs file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+const fetch = require('node-fetch');
 
 app.get('/', (req, res) => {
   res.render('login');
@@ -14,6 +15,23 @@ app.get('/create', (req, res) => {
 
 app.get('/test', (req, res) => {
   res.render('test');
+});
+
+app.get('/api', async (req, res) => {
+  let recipeNumber = 0;
+  let keyword = "chicken";
+  let url = `https://api.edamam.com/api/recipes/v2?type=public&q=${keyword}&app_id=f48ae2a3&app_key=%20eaa868f23b6bdf17d5281c6b0041671a`;
+  let response = await fetch(url);
+  let data = await response.json();
+  let numberOfRecipesFound = data.to;
+  let recipeLink = data.hits[recipeNumber].recipe.url;
+  let cuisineType = data.hits[recipeNumber].recipe.cuisineType;
+  let image = data.hits[recipeNumber].recipe.image;
+  let ingredientsArray = data.hits[recipeNumber].recipe.ingredientLines;
+  let recipieTitle = data.hits[recipeNumber].recipe.label;
+  let mealType = data.hits[recipeNumber].recipe.mealType[0];
+  let recipeInfo = [numberOfRecipesFound, recipeLink, cuisineType, image, ingredientsArray, recipieTitle, mealType]
+  res.render('apiTest', {"recipeInfo": recipeInfo});
 });
 
 app.listen(port, () => {
